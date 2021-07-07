cf81608f022426d7a247daed409efb722d651b92
Aki Tozawa <atozawa@jp.ibm.com>
2021-07-07 15:38:59 +0900

    Preparing pom.xml for windup integration

---
+++ windup/src/main/java/io/tackle/diva/windup/DivaLauncher.java
@@ -0,0 +1,12 @@
+package io.tackle.diva.windup;
+
+import org.jboss.windup.config.GraphRewrite;
+import org.jboss.windup.config.operation.GraphOperation;
+import org.ocpsoft.rewrite.context.EvaluationContext;
+
+public class DivaLauncher extends GraphOperation {
+    @Override
+    public void perform(GraphRewrite event, EvaluationContext context) {
+        // TODO Auto-generated method stub
+    }
+}
---
+++ windup/src/main/java/io/tackle/diva/windup/DivaRuleProvider.java
@@ -0,0 +1,24 @@
+package io.tackle.diva.windup;
+
+import org.jboss.windup.config.AbstractRuleProvider;
+import org.jboss.windup.config.loader.RuleLoaderContext;
+import org.jboss.windup.config.metadata.RuleMetadata;
+import org.jboss.windup.config.phase.MigrationRulesPhase;
+import org.ocpsoft.rewrite.config.Configuration;
+import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+
+/**
+*/
+@RuleMetadata(phase = MigrationRulesPhase.class)
+public class DivaRuleProvider extends AbstractRuleProvider {
+
+   // @formatter:off
+   @Override
+   public Configuration getConfiguration(RuleLoaderContext ruleLoaderContext)
+   {
+       return ConfigurationBuilder.begin()
+           .addRule()
+           .perform(new DivaLauncher());
+   }
+   // @formatter:on
+}
