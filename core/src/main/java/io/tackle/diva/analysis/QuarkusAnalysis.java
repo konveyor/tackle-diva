d011eb5ff94f6fc441e4763d9cf94e3183cd91eb
Aki Tozawa <atozawa@jp.ibm.com>
2022-04-21 22:41:22 +0900

    QuarkusAnalysis::checkRelevance

--- core/src/main/java/io/tackle/diva/analysis/QuarkusAnalysis.java
+++ core/src/main/java/io/tackle/diva/analysis/QuarkusAnalysis.java
@@ -25,6 +25,11 @@ import io.tackle.diva.Util;
 
 public class QuarkusAnalysis {
 
+    public static boolean checkRelevance(IClass c) {
+        return Util.any(Util.getAnnotations(c),
+                a -> a.getType().getName() == Constants.LMicroprofileReigsterRestClient);
+    }
+
     public static List<IMethod> getEntries(IClassHierarchy cha) throws IOException {
         List<IMethod> entries = new ArrayList<>();
 
